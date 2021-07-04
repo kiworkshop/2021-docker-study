e58028fa5cedc7da68b50b3954dd1754a8f84299
deocksoo <deocks@woowahan.com>
2021-07-04 18:52:42 +0900

    [deocks-2회차] Sqs continuous async Listener 구현

--- 2회차/deocks/web-app/src/main/java/kiworkshop/docker/webapp/sqs/MessageListener.java
+++ 2회차/deocks/web-app/src/main/java/kiworkshop/docker/webapp/sqs/MessageListener.java
@@ -1,12 +1,15 @@
 package kiworkshop.docker.webapp.sqs;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlRequest;
-import software.amazon.awssdk.services.sqs.model.GetQueueUrlResponse;
+import software.amazon.awssdk.services.sqs.model.*;
+
+import javax.annotation.PostConstruct;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 public class MessageListener {
@@ -19,16 +22,49 @@ public class MessageListener {
 
     public MessageListener(SqsAsyncClient sqsAsyncClient) {
         this.sqsAsyncClient = sqsAsyncClient;
-        this.queueUrl = getQueueUrl(sqsAsyncClient);
+        this.queueUrl = findQueueUrl(sqsAsyncClient);
     }
 
-    private String getQueueUrl(SqsAsyncClient sqsAsyncClient) {
-        GetQueueUrlResponse response = Mono.fromFuture(
-            sqsAsyncClient.getQueueUrl(GetQueueUrlRequest.builder()
-                .queueName(QUEUE_NAME)
-                .build())
-        ).block();
-        return response.queueUrl();
+    private String findQueueUrl(SqsAsyncClient sqsAsyncClient) {
+        GetQueueUrlRequest request = GetQueueUrlRequest.builder()
+            .queueName(QUEUE_NAME)
+            .build();
+        CompletableFuture<GetQueueUrlResponse> response = sqsAsyncClient.getQueueUrl(request);
+
+        return Mono.fromFuture(response)
+            .blockOptional()
+            .orElseThrow(() -> new RuntimeException("Failed to fetch queue name"))
+            .queueUrl();
     }
 
+    @PostConstruct
+    public void continuousListener() {
+        ReceiveMessageRequest request = ReceiveMessageRequest.builder()
+            .maxNumberOfMessages(5)
+            .queueUrl(queueUrl)
+            .waitTimeSeconds(10)
+            .visibilityTimeout(30)
+            .build();
+
+        Mono.fromFuture(() -> sqsAsyncClient.receiveMessage(request))
+            .repeat()
+            .retry()
+            .map(ReceiveMessageResponse::messages)
+            .flatMap(Flux::fromIterable)
+            .subscribe(message -> {
+                log.info("message body: {}", message.body());
+                delete(message);
+            });
+    }
+
+    private void delete(Message message) {
+        String receiptHandle = message.receiptHandle();
+        DeleteMessageRequest request = DeleteMessageRequest.builder()
+            .queueUrl(queueUrl)
+            .receiptHandle(receiptHandle)
+            .build();
+
+        Mono.fromFuture(sqsAsyncClient.deleteMessage(request))
+            .subscribe(response -> log.info("deleted message with handle : {}", receiptHandle));
+    }
 }
--- 2회차/deocks/web-app/src/main/java/kiworkshop/docker/webapp/sqs/SqsListenerHealthCheckController.java
+++ 2회차/deocks/web-app/src/main/java/kiworkshop/docker/webapp/sqs/SqsListenerHealthCheckController.java
@@ -24,4 +24,5 @@ public class SqsListenerHealthCheckController {
             return "No connected listener";
         }
     }
+
 }
